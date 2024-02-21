54b93921d1493602cb555f0d132ac47cd323b4d8
pingkakl <us13138765223@163.com>
2024-02-21 17:54:03 +0800

    hosts_search API implementation

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import "censys/src"
 
 func main() {
 	// 这里输入你要查询的域名
-	q := "198.23.246.120"
+	q := ""
 	// 提供你的 Censys API ID 和 Secret
-	apiID := "c37adb8f-eba1-4650-9426-8f8c61762233"
-	apiSecret := "sOkOmcofIxAvhPFMr7U0IY3K4ODUlZ9H"
+	apiID := ""
+	apiSecret := ""
 
 	//src.SearchCensys(q, apiID, apiSecret)
 	src.Ipv4Censys(q, apiID, apiSecret)
